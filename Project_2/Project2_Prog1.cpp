80350130f7f7e14f9b0e8f72eeed4429122ba99b
Louis Louis <louismonfi@gmail.com>
2019-02-18 12:53:05 -0800

    deb

--- Project_2/Project2_Prog1.cpp
+++ Project_2/Project2_Prog1.cpp
@@ -146,7 +146,7 @@ int areaCount(char array[][10], int x, int y, int c)
         areaCount(array, x, y + 1, c + 1);//check right 
         areaCount(array, x, y - 1, c + 1);//check left
         areaCount(array, x + 1, y, c + 1);//check up
-        c = areaCount(array, x - 1, y, c + 1);//check down
+        areaCount(array, x - 1, y, c + 1);//check down
         return c;
     }
     return c;
