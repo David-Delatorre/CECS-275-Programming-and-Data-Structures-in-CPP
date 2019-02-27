c6b1187e034c81b53434e509ae4a4e81b3da9da6
Louis Louis <louismonfi@gmail.com>
2019-02-27 12:52:29 -0800

    Final

--- Labs/Lab_5/Lab5.cpp
+++ Labs/Lab_5/Lab5.cpp
@@ -11,30 +11,67 @@
 #include <string>
 using namespace std;
 
+//Descr:
+//@param = Passes in a size (inputted by the user) of said array
 int * populate(int size);
+
+//Descr:
+//@param = Passes in a pointer to an array
+//@param = Passes in a size (inputted by the user) of said array
 void display(int *ptrArray, int size);
-// void swap(int num1, int num2);
-// void sort(int array[], int size);
-// void shuffle(int array[], int size);
-// int max(int array[], int size);
+
+//Descr:
+//@param = 
+void swap(int num1, int num2);
+
+//Descr:
+//@param = Passes in a pointer to an array
+//@param = Passes in a size (inputted by the user) of said array
+void sort(int *ptrArray, int size);
+
+//Descr:
+//@param = Passes in a pointer to an array
+//@param = Passes in a size (inputted by the user) of said array
+void shuffle(int *ptrArray, int size);
+
+//Descr:
+//@param = Passes in a pointer to an array
+//@param = Passes in a size (inputted by the user) of said array
+int max(int *ptrArray, int size);
+
+//Descr:
+//@param = 
 int GetPositiveInt();
+
+//Descr:
+//@param = 
 int GetIntRange(int low, int high);
+
+//Descr:
+//@param = 
 void menu();
+
 
 
 int main()
 {
-    //menu();
-    int arraySize;
-	cout << "Enter array size: ";
-    arraySize = GetPositiveInt();
+	int choice = 0;
 	int *ptrArray;
+	// bool loop = true;
+	cout << "Enter array size: ";
+    int arraySize = GetPositiveInt();
     ptrArray = populate(arraySize);
-	display(ptrArray, arraySize);
 
-
-
-
+	while (choice != 5)
+	{
+		menu();
+		choice = GetIntRange(1, 5);
+		if (choice == 1) 		display(ptrArray, arraySize);
+		else if (choice == 2)	sort(ptrArray, arraySize);
+		else if (choice == 3)	shuffle(ptrArray, arraySize);
+		else if (choice == 4)	cout << "Maximum number is: " << max(ptrArray, arraySize);
+		else if (choice == 5)	cout << "Program Exiting...\n";
+	}
 
     return 0;
 }
@@ -46,6 +83,7 @@ int * populate(int size)
     for (int i = 0; i < size; i++)
     {
         array[i] = (rand() % 100 + 1);
+		//cout << "\n" << array[i];
     }
     return array;
 }
@@ -53,10 +91,11 @@ int * populate(int size)
 void display(int *ptrArray, int size)
 {
     int counter = 0;
+	//cout << "test";
     for (int i = 0; i < size; i++)
     {
-        if ((i + 1) != size) cout << *ptrArray[i] << ", ";
-        else cout << *ptrArray[i];
+        if ((i + 1) != size) cout << ptrArray[i] << ", ";
+        else cout << ptrArray[i];
         
         counter++;
         if (counter == 10)
@@ -67,25 +106,52 @@ void display(int *ptrArray, int size)
     }
 }
 
-// void swap(int num1, int num2)
-// {
-//     //code
-// }
+void swap(int *num1, int *num2)
+{
+    int temp;
+	temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
 
-// void sort(int array[], int size)
-// {
-//     //code
-// }
+void sort(int *ptrArray, int size)
+{
+	int min;
+    for (int i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (int j = i + 1; j < size; j++)
+		{
+			if (ptrArray[j] < ptrArray[min])	min = j;
+		}
+		swap(&ptrArray[min], &ptrArray[i]);
+	}
+}
 
-// void shuffle(int array[], int size)
-// {
-//     //code
-// }
+void shuffle(int *ptrArray, int size)
+{
+	srand(time(NULL));
+	int ind1, ind2;
+    for (int i = 0; i < size*10; i++)
+	{
+		ind1 = (rand() % size);
+		ind2 = (rand() % size);
+		swap(&ptrArray[ind1], &ptrArray[ind2]);
+	}
+}
 
-// int max(int array[], int size)
-// {
-//     //code
-// }
+int max(int *ptrArray, int size)
+{
+	int maximum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (maximum < ptrArray[i])
+		{
+			maximum = ptrArray[i];
+		}
+	}
+	return maximum;
+}
 
 int GetPositiveInt()
 {
